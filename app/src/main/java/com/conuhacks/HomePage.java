a29deb926f2f802fc6c0b3b8e594bd77714a33c7
Kevin Huang <kevin2huang@hotmail.com>
2016-01-23 16:34:58 -0500

    added dropdown for radius added find beer button

--- app/src/main/java/com/conuhacks/HomePage.java
+++ app/src/main/java/com/conuhacks/HomePage.java
@@ -1,13 +1,39 @@
 package com.conuhacks;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomePage extends AppCompatActivity {
-
+public class HomePage extends Activity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_page);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Radius, android.R.layout.simple_spinner_item );
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView text = (TextView) view;
+        Toast.makeText(this, "You Selected "+ text.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
